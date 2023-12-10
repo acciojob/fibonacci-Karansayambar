b227212f0fcc80b3ab32c75d45b7cdd3aab9666f
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-10 09:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function fibonacci(num) {
-// your code here
-	if (num < 0 ){
-        return "Invalid input. Please enter a positive integer between 0 and 50 (inclusive).";
-    }
-
-    let a = 0, b = 1;
-
-    for (let i = 2; i <= num; i++) {
-        let temp = a + b;
-        a = b;
-        b = temp;
-    }
-
-    return a;
+	if(n > 0){
+		return "please enter valid input";
+	}
+	let a = 0, b = 1;
+	for(let i = 2; i <= 2; i++){
+		let temp = a + b;
+		a = b;
+		b = temp;
+	}
+	return a;
 }
 module.exports = fibonacci;
